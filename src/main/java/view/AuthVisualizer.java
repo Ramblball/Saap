a67f0f862dafac258ed01f3d6ec7ecf6b0385937
Maxim <lostrandg@gmail.com>
2021-04-26 10:23:20 +0500

    feature(view): добавление страницы авторизации

---
+++ src/main/java/view/AuthVisualizer.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AuthVisualizer extends JPanel {
+
+    public AuthVisualizer() {
+        Label label = new Label("");
+        label.setBounds(5, 50, 150, 30);
+
+    }
+}
