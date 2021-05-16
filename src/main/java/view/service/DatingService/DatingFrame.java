963d7422db0db8e7ef26ab1d8281f4df15330cea
Tempest <Misha8936@yandex.ru>
2021-05-16 15:48:19 +0500

    feature(dating): добавление контроллера, запроса по городу

--- src/main/java/view/service/DatingService/DatingFrame.java
+++ src/main/java/view/service/DatingService/DatingFrame.java
@@ -1,5 +1,10 @@
 package view.service.DatingService;
 
+import controller.DatingController;
+import controller.exceptions.NotFoundException;
+import http.payload.DatingUser;
+import model.User;
+import view.main.MainFrame;
 import view.service.DatingService.Frame;
 import view.ViewLiterals;
 
@@ -12,6 +17,7 @@ public class DatingFrame extends JFrame implements Frame {
     private final JButton likeButton = new JButton();
     private final JButton chatButton = new JButton();
     private final JButton skipButton = new JButton();
+    private final JTextField cityName = new JTextField();
 
     public DatingFrame(){
         super("Dating app");
@@ -22,13 +28,15 @@ public class DatingFrame extends JFrame implements Frame {
     public void setComponentsStyle() {
         informationPanel.setBounds(50, 28, 304, 345);
         informationPanel.setBackground(new Color(196, 196, 196));
-        likeButton.setText("LIKE");
+        likeButton.setText("LIKE"); //Предполагается убрать текст когда на кнопках будут картинки
         chatButton.setText("CHAT");
         skipButton.setText("SKIP");
         likeButton.setBounds(59, 412, 71, 70);
         chatButton.setBounds(162, 412, 72, 72);
         skipButton.setBounds(273, 412, 72, 72);
         backButton.setBounds(57, 515, 290, 50);
+        cityName.setBounds(100, 380, 200, 30);
+        cityName.setText("Город");
     }
 
     @Override
@@ -39,12 +47,24 @@ public class DatingFrame extends JFrame implements Frame {
         this.add(likeButton);
         this.add(backButton);
         this.add(informationPanel);
+        this.add(cityName);
     }
 
     @Override
     public void addListeners() {
         backButton.addActionListener(e -> {
             dispose();
+        });
+        skipButton.addActionListener(e -> {
+            DatingUser datingUser = new DatingUser(cityName.getText());
+            User user;
+            try {
+                user = DatingController.GetUserList(datingUser);
+            } catch (NotFoundException notFoundException) {
+                JOptionPane.showMessageDialog(this, notFoundException.getMessage());
+                return;
+            }
+
         });
     }
 
